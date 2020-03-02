efa331588ab41d12723c833e477d317584cf8c49
MD Monirul Islam <mdmonirul.islam@ibm.com>
2020-03-01 21:30:54 -0500

    1st commit

---
+++ question.js
@@ -0,0 +1,35 @@
+var questions = [{
+	"question": "What does CSS stand for?",
+	"option1": " Cascading Style Sheets",
+	"option2": "Computer Style Sheets",
+	"option3": "Creative Style Sheets",
+	"option4": " Colorful Style Sheets",
+	"answer": "1"
+}, {
+
+	"question": "What is the correct HTML for referring to an external style sheet?",
+	"option1": "<stylesheet>mystyle.css</stylesheet>",
+	"option2": "<style src='mystyle.css'>",
+	"option3": "<link rel='stylesheet' type = 'text/css' href='mysctyles.css'>",
+	"option4": "None of these",
+	"answer": "3"
+}, {
+
+	"question": "Which HTML tag is used to define an internal style sheet?",
+	"option1": "<style>",
+	"option2": "<script>",
+	"option3": "<css>",
+	"option4": "none of these",
+	"answer": "1"
+},
+{
+
+	"question": "Which HTML attribute is used to define inline styles",
+	"option1": "Styles",
+	"option2": "Style",
+	"option3": "CSS",
+	"option4": "Class",
+	"answer": "2"
+}
+
+]
